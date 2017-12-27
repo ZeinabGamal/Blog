92a2d8287a559dfdd81f565e6cc16b6d12427089
Zeinab Hamed <zeinabmohammed950@gmail.com>
2017-12-27 16:41:44 +0200

    add

--- MySite/settings.py
+++ MySite/settings.py
@@ -122,7 +122,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+                os.path.join(BASE_DIR, "static"),
+            ]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = '/'  #redirect to home page when login
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
